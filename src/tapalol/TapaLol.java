03c981c96030a04cd9e81688f9252b9fb3dae341
Gabriel Amorim <gaahamorim95@gmail.com>
2019-04-26 09:46:35 -0300

    Criação Projeto + Tela Inicio + Tela Jogo

---
+++ src/tapalol/TapaLol.java
@@ -0,0 +1,15 @@
+package tapalol;
+
+/**
+ *
+ * @author Gabriel Amorim
+ * @author Felipe Toshio
+ */
+public class TapaLol {
+
+    public static void main(String[] args) {
+        TelaInicio TapaLol = new TelaInicio();
+        TapaLol.setVisible(true);
+    }
+    
+}
